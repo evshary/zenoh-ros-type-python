71d554fea804249a9b4f6cd79e6e9e2f4b665e19
DennyKuo0809 <ktying8989@gmail.com>
2024-04-01 10:45:02 +0800

    Add geographic_info and autoware_adapi_msgs

---
+++ zenoh_ros_type/geographic_info/geographic_msgs.py
@@ -0,0 +1,13 @@
+from pycdr2.types import float64
+from ..common_interfaces.std_msgs import Header
+
+@dataclass
+class GeoPoint(IdlStruct, typename="GeoPoint"):
+    latitude: float64
+    longitude: float64
+    altitude: float64
+
+@dataclass
+class GeoPointStamped(IdlStruct, typename="GeoPointStamped"):
+    header: Header
+    position: GeoPoint
