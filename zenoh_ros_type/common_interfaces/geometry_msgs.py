412f0f2a2efbb9a93c1209b32764424d5fd5361e
DennyKuo0809 <ktying8989@gmail.com>
2024-04-01 08:32:36 +0800

    Add type into geometry_msgs

--- zenoh_ros_type/common_interfaces/geometry_msgs.py
+++ zenoh_ros_type/common_interfaces/geometry_msgs.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from pycdr2 import IdlStruct
-from pycdr2.types import float32, float64
+from pycdr2.types import float32, float64, array
+from .std_msgs import Header
 
 @dataclass
 class Point(IdlStruct, typename="Point"):
@@ -27,6 +28,21 @@ class Pose(IdlStruct, typename="Pose"):
     orientation: Quaternion
 
 @dataclass
+class PoseStamped(IdlStruct, typename="PoseStamped"):
+    header: Header
+    pose: Pose
+
+@dataclass
+class PoseWithCovariance(IdlStruct, typename="PoseWithCovariance"):
+    pose: Pose
+    covariance: array[float64, 36]
+
+@dataclass
+class PoseWithCovarianceStamped(IdlStruct, typename="PoseWithCovarianceStamped"):
+    header: Header
+    pose: PoseWithCovariance
+
+@dataclass
 class Vector3(IdlStruct, typename="Vector3"):
     x: float64
     y: float64
@@ -36,3 +52,28 @@ class Vector3(IdlStruct, typename="Vector3"):
 class Twist(IdlStruct, typename="Twist"):
     linear: Vector3
     angular: Vector3
+
+@dataclass
+class TwistWithCovariance(IdlStruct, typename="TwistWithCovariance"):
+    twist: Twist
+    covariance: array[float64, 36]
+
+@dataclass
+class TwistWithCovarianceStamped(IdlStruct, typename="TwistWithCovarianceStamped"):
+    header: Header
+    twist: TwistWithCovariance
+
+@dataclass
+class Accel(IdlStruct, typename="Accel"):
+    linear: Vector3
+    angular: Vector3
+
+@dataclass
+class AccelWithCovariance(IdlStruct, typename="AccelWithCovariance"):
+    accel: Accel
+    covariance: array[float64, 36]
+
+@dataclass
+class AccelWithCovarianceStamped(IdlStruct, typename="AccelWithCovarianceStamped"):
+    header: Header
+    accel: AccelWithCovariance
