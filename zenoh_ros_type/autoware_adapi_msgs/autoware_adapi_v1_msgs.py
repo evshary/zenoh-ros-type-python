e945ae054d427e9f68cc478082aef2c19f1640cc
Leann <leann9001@gmail.com>
2024-07-23 17:43:18 +0800

    add types to change operation mode

--- zenoh_ros_type/autoware_adapi_msgs/autoware_adapi_v1_msgs.py
+++ zenoh_ros_type/autoware_adapi_msgs/autoware_adapi_v1_msgs.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from pycdr2 import IdlStruct
+from pycdr2 import IdlStruct, Enum
 from ..geographic_info.geographic_msgs import GeoPointStamped
 from ..common_interfaces.geometry_msgs import PoseWithCovarianceStamped, TwistWithCovarianceStamped, AccelWithCovarianceStamped
 
@@ -9,3 +9,25 @@ class VehicleKinematics(IdlStruct, typename="VehicleKinematics"):
     pose: PoseWithCovarianceStamped
     twist: TwistWithCovarianceStamped
     accel: AccelWithCovarianceStamped
+
+@dataclass
+class ResponseStatus(IdlStruct, typename="ResponseStatus"):
+    class CODE(Enum):
+        # Error codes
+        UNKNOWN = 50000
+        SERVICE_UNREADY = 50001
+        SERVICE_TIMEOUT = 50002
+        TRANSFORM_ERROR = 50003
+        PARAMETER_ERROR = 50004
+
+        # Warning codes
+        DEPRECATED = 60000
+        NO_EFFECT = 60001
+
+    success: bool
+    code: int
+    message: str
+
+@dataclass
+class ChangeOperationMode(IdlStruct, typename="ChangeOperationMode"):
+    status: ResponseStatus
