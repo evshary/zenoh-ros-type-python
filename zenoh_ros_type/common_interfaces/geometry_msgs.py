1025bce8696258df37f573af6c8180506ac7191b
ChenYing Kuo <evshary@gmail.com>
2026-01-06 15:04:21 +0800

    feat: Add new geometry_msgs types including stamped versions, transforms, wrenches, and inertia.
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- zenoh_ros_type/common_interfaces/geometry_msgs.py
+++ zenoh_ros_type/common_interfaces/geometry_msgs.py
@@ -1,9 +1,22 @@
 from dataclasses import dataclass
 
 from pycdr2 import IdlStruct
-from pycdr2.types import array, float32, float64
+from pycdr2.types import array, float32, float64, int64, sequence
 
 from .std_msgs import Header
+
+
+@dataclass
+class Vector3(IdlStruct, typename='Vector3'):
+    x: float64
+    y: float64
+    z: float64
+
+
+@dataclass
+class Vector3Stamped(IdlStruct, typename='Vector3Stamped'):
+    header: Header
+    vector: Vector3
 
 
 @dataclass
@@ -21,11 +34,23 @@ class Point32(IdlStruct, typename='Point32'):
 
 
 @dataclass
+class PointStamped(IdlStruct, typename='PointStamped'):
+    header: Header
+    point: Point
+
+
+@dataclass
 class Quaternion(IdlStruct, typename='Quaternion'):
     x: float64
     y: float64
     z: float64
     w: float64
+
+
+@dataclass
+class QuaternionStamped(IdlStruct, typename='QuaternionStamped'):
+    header: Header
+    quaternion: Quaternion
 
 
 @dataclass
@@ -41,6 +66,12 @@ class PoseStamped(IdlStruct, typename='PoseStamped'):
 
 
 @dataclass
+class PoseArray(IdlStruct, typename='PoseArray'):
+    header: Header
+    poses: sequence[Pose]
+
+
+@dataclass
 class PoseWithCovariance(IdlStruct, typename='PoseWithCovariance'):
     pose: Pose
     covariance: array[float64, 36]
@@ -53,16 +84,28 @@ class PoseWithCovarianceStamped(IdlStruct, typename='PoseWithCovarianceStamped')
 
 
 @dataclass
-class Vector3(IdlStruct, typename='Vector3'):
-    x: float64
-    y: float64
-    z: float64
+class Transform(IdlStruct, typename='Transform'):
+    translation: Vector3
+    rotation: Quaternion
+
+
+@dataclass
+class TransformStamped(IdlStruct, typename='TransformStamped'):
+    header: Header
+    child_frame_id: str
+    transform: Transform
 
 
 @dataclass
 class Twist(IdlStruct, typename='Twist'):
     linear: Vector3
     angular: Vector3
+
+
+@dataclass
+class TwistStamped(IdlStruct, typename='TwistStamped'):
+    header: Header
+    twist: Twist
 
 
 @dataclass
@@ -84,6 +127,12 @@ class Accel(IdlStruct, typename='Accel'):
 
 
 @dataclass
+class AccelStamped(IdlStruct, typename='AccelStamped'):
+    header: Header
+    accel: Accel
+
+
+@dataclass
 class AccelWithCovariance(IdlStruct, typename='AccelWithCovariance'):
     accel: Accel
     covariance: array[float64, 36]
@@ -93,3 +142,64 @@ class AccelWithCovariance(IdlStruct, typename='AccelWithCovariance'):
 class AccelWithCovarianceStamped(IdlStruct, typename='AccelWithCovarianceStamped'):
     header: Header
     accel: AccelWithCovariance
+
+
+@dataclass
+class Wrench(IdlStruct, typename='Wrench'):
+    force: Vector3
+    torque: Vector3
+
+
+@dataclass
+class WrenchStamped(IdlStruct, typename='WrenchStamped'):
+    header: Header
+    wrench: Wrench
+
+
+@dataclass
+class Inertia(IdlStruct, typename='Inertia'):
+    m: float64
+    com: Vector3
+    ixx: float64
+    ixy: float64
+    ixz: float64
+    iyy: float64
+    iyz: float64
+    izz: float64
+
+
+@dataclass
+class InertiaStamped(IdlStruct, typename='InertiaStamped'):
+    header: Header
+    inertia: Inertia
+
+
+@dataclass
+class Polygon(IdlStruct, typename='Polygon'):
+    points: sequence[Point32]
+
+
+@dataclass
+class PolygonStamped(IdlStruct, typename='PolygonStamped'):
+    header: Header
+    polygon: Polygon
+
+
+@dataclass
+class PolygonInstance(IdlStruct, typename='PolygonInstance'):
+    polygon: Polygon
+    id: int64
+
+
+@dataclass
+class PolygonInstanceStamped(IdlStruct, typename='PolygonInstanceStamped'):
+    header: Header
+    polygon: PolygonInstance
+
+
+@dataclass
+class VelocityStamped(IdlStruct, typename='VelocityStamped'):
+    header: Header
+    body_frame_id: str
+    reference_frame_id: str
+    velocity: Twist
