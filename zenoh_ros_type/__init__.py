fd4a71a0dffa198c59086346db0f5aa8a46676f0
Leann Hsu <leann9001@gmail.com>
2026-01-15 23:49:44 +0800

    Add rmw_zenoh attachment class

--- zenoh_ros_type/__init__.py
+++ zenoh_ros_type/__init__.py
@@ -4,6 +4,7 @@ from .autoware_msgs import *  # NOQA
 from .common_interfaces import *  # NOQA
 from .example_interfaces import *  # NOQA
 from .rcl_interfaces import *  # NOQA
+from .rmw_zenoh import *  # NOQA
 from .tier4_autoware_msgs import *  # NOQA
 from .unique_identifier_msgs import *  # NOQA
 from .service import *  # NOQA
