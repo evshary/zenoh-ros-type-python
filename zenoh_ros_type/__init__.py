f5250e79428ef2f0f372d77a110e57da14ef3477
Leann Hsu <leann9001@gmail.com>
2026-01-16 19:03:09 +0800

    Add rmw_zenoh support and fix subpackage discovery (#19)
    * Add rmw_zenoh attachment class
    
    * Fix package discovery to include subpackages
    
    * Add Empty for rmw_zenoh payload

--- zenoh_ros_type/__init__.py
+++ zenoh_ros_type/__init__.py
@@ -1,9 +1,12 @@
 from .action import *  # NOQA
+from .autoware_adapi_msgs import *  # NOQA
 from .autoware_auto_msgs import *  # NOQA
 from .autoware_msgs import *  # NOQA
 from .common_interfaces import *  # NOQA
 from .example_interfaces import *  # NOQA
+from .geographic_info import *  # NOQA
 from .rcl_interfaces import *  # NOQA
+from .rmw_zenoh import *  # NOQA
 from .tier4_autoware_msgs import *  # NOQA
 from .unique_identifier_msgs import *  # NOQA
 from .service import *  # NOQA
---
+++ zenoh_ros_type/rmw_zenoh.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+
+from pycdr2 import IdlStruct
+from pycdr2.types import array, int64, uint8
+
+
+@dataclass
+class Attachment(IdlStruct, typename='Attachment'):
+    """
+    rmw_zenoh attachment (33 bytes).
+
+    - sequence_number: 8 bytes, little-endian int64
+    - timestamp_ns: 8 bytes, little-endian int64
+    - gid_length: 1 byte (always 16)
+    - gid: 16 bytes
+
+    Source: https://github.com/ros2/rmw_zenoh/blob/humble/docs/design.md
+    """
+
+    sequence_number: int64
+    timestamp_ns: int64
+    gid_length: uint8
+    gid: array[uint8, 16]
+
+    def serialize(self) -> bytes:
+        # Strip the 4-byte CDR header added by pycdr2 (rmw_zenoh expects raw bytes)
+        return super().serialize()[4:]
+
+    @classmethod
+    def deserialize(cls, data: bytes) -> 'Attachment':
+        # Prepend CDR header for pycdr2 to parse
+        # \x00\x01\x00\x00 = little-endian, CDR version 1, options 0
+        cdr_data = b'\x00\x01\x00\x00' + data
+        return super(Attachment, cls).deserialize(cdr_data)
+
+
+@dataclass
+class Empty(IdlStruct, typename='Empty'):
+    # rmw_zenoh does not accept 0-byte payload, requires 1 byte padding
+    padding: uint8 = 0
